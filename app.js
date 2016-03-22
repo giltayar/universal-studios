cf930d7923b970017234ea27f04df2a1e12e589b
Gil Tayar <gilt@wix.com>
2016-03-22 20:16:19 +0200

    react rendering now works server-side

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const ReactDomServer = require('react-dom/server')
+const helloWorld = require('./src/public/hello.jsx')
 
 const app = express()
 
-app.use('/index.html', express.static(__dirname + '/src/public/index.html'));
+app.use('/index.html', (req, res) => res.send(`
+<html>
+    <body>
+        <div id="root">
+            ${ReactDomServer.renderToString(helloWorld)}
+        </div>
+        <script src="bundle.js"></script>
+    </body>
+ </html>
+`))
+
 app.use('/bundle.js', express.static(__dirname + '/lib/public/bundle.js'));
 
 app.listen(process.env.PORT || 3000, () => {
